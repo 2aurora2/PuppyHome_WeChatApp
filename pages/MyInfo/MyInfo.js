bc301d5fc2e256cb5c21aed1f0444e7250fec9ec
WA_automat <1577696824@qq.com>
2023-03-02 13:41:33 +0800

    debug avatar

--- pages/MyInfo/MyInfo.js
+++ pages/MyInfo/MyInfo.js
@@ -2,21 +2,25 @@ const app = getApp();
 
 Page({
   data: {
-    AvatarUrl: app.globalData.userInfo.avatar,
+    AvatarUrl: "",
     bordeStyle: "",
   },
   onLoad() {
     // 用户身份标识
+    console.log(app.globalData.userInfo.avatar)
     if (app.globalData.userInfo.authentication == 0) {
       this.setData({
+        AvatarUrl: app.globalData.userInfo.avatar,
         bordeStyle: "5px lightslategray solid" //普通用户“灰色”标识
       })
     } else if (app.globalData.userInfo.authentication == 1) {
       this.setData({
+        AvatarUrl: app.globalData.userInfo.avatar,
         bordeStyle: "5px deepskyblue solid" //管理员“蓝色”标识
       })
     } else if (app.globalData.userInfo.authentication == 2) {
       this.setData({
+        AvatarUrl: app.globalData.userInfo.avatar,
         bordeStyle: "5px firebrick solid" //超级管理员“红色”标识
       })
     }

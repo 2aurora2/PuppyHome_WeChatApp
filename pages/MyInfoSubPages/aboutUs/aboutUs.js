28f6b203d7ea484be5ba765b591b73c1dfef8db8
2aurora2 <1971261337@qq.com>
2023-03-04 19:57:58 +0800

    AboutUs Revise

--- pages/MyInfoSubPages/aboutUs/aboutUs.js
+++ pages/MyInfoSubPages/aboutUs/aboutUs.js
@@ -1,6 +1,6 @@
 const HtmlTitle = '<h1 style="color: #483D8B">修勾之家</h1>'
 const HtmlSubTitle1 = '<h3 style="color: #483D8B">一、平台介绍</h3>'
-const HtmlSubTitle2 = '<h3 style="color: #483D8B">二、用户规范</h3>'
+const HtmlSubTitle2 = '<h3 style="color: #483D8B">二、用户说明</h3>'
 const HtmlSubTitle21 = '<h3 style="color: #483D8B">2.1 普通用户</h3>'
 const HtmlSubTitle22 = '<h3 style="color: #483D8B">2.2 普通管理员</h3>'
 const HtmlSubTitle23 = '<h3 style="color: #483D8B">2.3 超级管理员</h3>'

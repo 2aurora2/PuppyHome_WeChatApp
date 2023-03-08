0a44ae18c88c7cf2b53c56853b371a054764318c
2aurora2 <1971261337@qq.com>
2023-03-08 14:48:23 +0800

    articleList request

--- pages/home/home.js
+++ pages/home/home.js
@@ -53,22 +53,51 @@ Page({
     this.setData({
       isshow: app.globalData.hasEvenLogin
     });
-    // 页面懒加载逻辑
-    var currentList = this.data.currentArticleList;
-    var currentN = this.data.currentNum;
-    if (this.data.articleList.length - currentList.length < this.data.perShow) {
-      this.setData({
-        currentArticleList: this.data.currentArticleList
-      })
-    } else {
-      for (var i = currentN; i < currentN + this.data.perShow; i++) {
-        currentList.push(this.data.articleList[i])
+    var that = this;
+    // 请求文章列表数据
+    wx.request({
+      url: 'http://localhost:3000/article/except',
+      method: 'GET',
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      data: {
+        token: wx.getStorageSync('token')
+      },
+      success(res) {
+        var articles = res.data.data.articles;
+        var dogs = res.data.data.dogs;
+        var finalList = [];
+        for (var i = 0; i < articles.length; i++) {
+          var obj = {};
+          obj['id'] = articles[i].id;
+          obj['title'] = articles[i].title;
+          obj['publishTime'] = articles[i].publishTime;
+          obj['photo'] = dogs[i].photo;
+          finalList.push(obj);
+        }
+        that.setData({
+          articleList: finalList
+        })
+        // 页面懒加载逻辑
+        var currentList = that.data.currentArticleList;
+        var currentN = that.data.currentNum;
+        if (that.data.articleList.length < that.data.perShow) {
+          that.setData({
+            currentArticleList: that.data.articleList,
+            currentNum : that.data.articleList.length
+          })
+        } else {
+          for (var i = currentN; i < currentN + that.data.perShow; i++) {
+            currentList.push(that.data.articleList[i])
+          }
+          that.setData({
+            currentArticleList: currentList,
+            currentNum: currentN + that.data.perShow
+          })
+        }
       }
-      this.setData({
-        currentArticleList: currentList,
-        currentNum: currentN + this.data.perShow
-      })
-    }
+    })
   },
   // 上拉触底lazy load
   onReachBottom() {
